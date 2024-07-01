bc89a880f0cd45879a82a51780839bfc2b9afdaf
maoshuo chen <maoshuochen@gmail.com>
2024-07-02 00:07:29 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -10,12 +10,12 @@ const CONFIG = {
     github: "maoshuochen",
     instagram: "",
   },
-  // projects: [
-  //   {
-  //     name: `morethan-log`,
-  //     href: "https://github.com/morethanmin/morethan-log",
-  //   },
-  // ],
+  projects: [
+    {
+      name: `maoshuochen`,
+      href: "https://maoshuochen.top/",
+    },
+  ],
   // blog setting (required)
   blog: {
     title: "morethan-log",
